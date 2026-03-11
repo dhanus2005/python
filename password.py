2ef3039aed9764895aa828873060791f6decce1a
dhanus2005 <dhanuskaarthikeyan27@gmail.com>
2026-03-11 15:22:16 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "dhanus" and password == "2005":
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
